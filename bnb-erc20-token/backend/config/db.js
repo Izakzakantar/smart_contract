006fd1e6caffb3bf140f26d5d762350a42484f6f
Izakzakantar <belghitishakantar@gmail.com>
2024-09-11 17:42:35 +0100

    donor.js modified

--- bnb-erc20-token/backend/config/db.js
+++ bnb-erc20-token/backend/config/db.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     host:"localhost",
     dialect:"mysql",
+    port:3306,
     logging:console.log
+    
 })
 module.exports=sequelize;
--- bnb-erc20-token/backend/models/Donor.js
+++ bnb-erc20-token/backend/models/Donor.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
-const User = require('..models/user');
+const User = require('../models/User');
 
 const Donor = sequelize.define('Donor', {
   donor_id: {
